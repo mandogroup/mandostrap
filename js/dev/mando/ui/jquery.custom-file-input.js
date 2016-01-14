21d1bca389a5683e8439c7483c1396013bb6640d
Heidi Crook <heidi@heidiworld.co.uk>
2016-01-14 12:24:27 +0000

    remove button added to form  file input feature

--- js/dev/mando/ui/jquery.custom-file-input.js
+++ js/dev/mando/ui/jquery.custom-file-input.js
@@ -2,37 +2,50 @@
 	By Osvaldas Valutis, www.osvaldas.info
 	Available for use under the MIT License
 */
+; (function ($, window, document, undefined) {
 
-'use strict';
+    $('.inputfile').each(function () {
+        var $input = $(this),
+            $label = $input.next('label'),
+             labelVal = $label.html();
 
-;( function($, window, document, undefined ){
-	
-	$('.inputfile').each( function(){
-		  var $input = $(this),
-					$label = $input.next( 'label' ),
-					labelVal = $label.html();
+        //Reset function
+        function resetFile() {
+            $label.html(labelVal);
+            $label.next($('.js-remove')).remove();
+            //refire grunticon otherwise icon disappears
+            grunticon.embedIcons(grunticon.getIcons(grunticon.getCSS(grunticon.href)));
 
-		$input.on( 'change', function( e ){
-			var fileName = '';
+        }
 
-			if( this.files && this.files.length > 1 )
-				fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-			else if( e.target.value )
-				fileName = e.target.value.split( '\\' ).pop();
+        $input.on('change', function (e) {
+            var fileName = '';
 
-			if( fileName )
-				$label.find( 'span' ).html(fileName);
-			else
-				$label.html( labelVal );
-		});
+            if (this.files && this.files.length > 1)
+                fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+            else if (e.target.value)
+                fileName = e.target.value.split('\\').pop();
 
-		// Firefox bug fix
-		$input.on('focus', function(){ 
-			$input.addClass( 'has-focus' ); 
-		}).on( 'blur', function(){ 
-			$input.removeClass( 'has-focus' ); 
-		});
+            if (fileName) {
+                $label.find('span').html(fileName);
+                $('<button type="button" class="c-textbutton inputfile__remove js-remove">Remove document</button>').insertAfter($label);
+            }
+            else {
+                resetFile();
+            }
+            $('.js-remove').on("click", function () {
+                $input.val('');
+                resetFile();
+            });
+        });
 
-	});
-	
-})( jQuery, window, document );
+        // Firefox bug fix
+        $input.on('focus', function () {
+            $input.addClass('has-focus');
+        }).on('blur', function () {
+            $input.removeClass('has-focus');
+        });
+
+    });
+
+})(jQuery, window, document);
