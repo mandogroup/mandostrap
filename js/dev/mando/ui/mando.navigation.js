8c53b22541aee478c5d1fefc9f2db743a80bddf5
Simon Melhuish <simondavidmelhuish@googlemail.com>
2016-02-09 16:26:05 +0000

    Navigation
    Added some CSS and also updated the logic to show/hide the menu with
    button clicks on smaller screens.

--- js/dev/mando/ui/mando.navigation.js
+++ js/dev/mando/ui/mando.navigation.js
@@ -1,8 +1,24 @@
 
 navigation = function () {
 
-  var element = {
+    // Grab our toggle and it's linked target
+    var navToggle = $("[data-toggle='button']"),
+    navTarget = navToggle.data('target');
 
-  };
+    // On click toggle active classes to both
+    navToggle.on("click", function(){
+        $(this).toggleClass("active");
+        $(navTarget).toggleClass("active");
+    });
+
+    // Stopping propogation isn't a good idea so we do this to detect
+    // page clicks and close the menu, see the link below for details.
+    // https://css-tricks.com/dangers-stopping-event-propagation/
+    $(document).on('click', function(event) {
+      if (!$(event.target).closest('.c-navigation').length) {
+        $(navToggle).removeClass("active");
+        $(navTarget).removeClass("active");
+      }
+    });
 
 };
