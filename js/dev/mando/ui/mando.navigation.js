91546f76e70393870a68236b845c40b29db1ba79
Simon Melhuish <simondavidmelhuish@googlemail.com>
2016-02-10 13:15:10 +0000

    Update to navigation component
    Some styles changes, update to markup and all JS controls added.

--- js/dev/mando/ui/mando.navigation.js
+++ js/dev/mando/ui/mando.navigation.js
@@ -1,24 +1,47 @@
 
 navigation = function () {
 
-    // Grab our toggle and it's linked target
-    var navToggle = $("[data-toggle='button']"),
-    navTarget = navToggle.data('target');
+    // Initial Variables
+    // -------------------------------------------------------------
 
-    // On click toggle active classes to both
+    var navToggle = $("[data-toggle='button']"),
+        navTarget = navToggle.data('target'),
+        dropToggle = $("[data-toggle='dropdown']");
+
+    // Mobile Menu
+    // -------------------------------------------------------------
+
     navToggle.on("click", function(){
-        $(this).toggleClass("active");
-        $(navTarget).toggleClass("active");
+        $(this).toggleClass("is-active");
+        $(navTarget).toggleClass("is-active");
     });
 
+    // Mega Menu / Dropdown
+    // -------------------------------------------------------------
+
+    dropToggle.on("click", function(){
+        $(this).parent().siblings().removeClass('is-active');
+        $(this).parent().toggleClass('is-active');
+    });
+
+    // Catching Clicks
+    // -------------------------------------------------------------
     // Stopping propogation isn't a good idea so we do this to detect
     // page clicks and close the menu, see the link below for details.
     // https://css-tricks.com/dangers-stopping-event-propagation/
+
     $(document).on('click', function(event) {
       if (!$(event.target).closest('.c-navigation').length) {
-        $(navToggle).removeClass("active");
-        $(navTarget).removeClass("active");
+        $('.c-navigation').find('.is-active').removeClass('is-active');
       }
+    });
+
+    // Window Resize
+    // -------------------------------------------------------------
+    // Hides all dropdowns when we resize the browser
+
+    $(window).on('resize', function(){
+        $('.c-navigation').find('.is-active').removeClass('is-active');
     });
 
 };
