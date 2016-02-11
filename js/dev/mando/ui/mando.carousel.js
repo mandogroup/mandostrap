f48945045ebe8956bf44511dd5700c2323ec4aa8
Simon Melhuish <simondavidmelhuish@googlemail.com>
2016-02-11 17:17:30 +0000

    Split carousel file to allow separate JS for hero carousel

--- js/dev/mando/ui/mando.carousel.js
+++ js/dev/mando/ui/mando.carousel.js
@@ -36,12 +36,4 @@ carousel = function () {
     ]
   });
 
-  $('.js-hero-slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: false
-  });
 };
