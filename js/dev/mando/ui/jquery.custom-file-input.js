64d18efd144da49a9316336c935eae8f322800b1
Heidi Crook <heidi@heidiworld.co.uk>
2016-02-18 15:03:24 +0000

    bug fix and naming convention

--- js/dev/mando/ui/jquery.custom-file-input.js
+++ js/dev/mando/ui/jquery.custom-file-input.js
@@ -3,15 +3,15 @@
 	Available for use under the MIT License
 */
 ; (function ($, window, document, undefined) {
-    $('.inputfile').each(function () {
-        var $input = $(this),
-            $label = $input.next('label'),
-             labelVal = $label.html();
 
-        //Reset function
+   $('.js-inputfile').each(function () {
+        var $input = $(this),
+			$label = $input.next('label'),
+			labelVal = $label.html();
+
         function resetFile() {
             $label.html(labelVal);
-            $label.next($('.js-remove')).remove();
+            $label.next('.js-remove').remove();
             //refire grunticon otherwise icon disappears
             grunticon.embedIcons(grunticon.getIcons(grunticon.getCSS(grunticon.href)));
 
@@ -27,26 +27,27 @@
 
             if (fileName) {
                 $label.find('span').html(fileName);
-                 if (!$('.js-remove')[0]) {
+                if (!$('.js-remove')[0]) {
                      $('<button type="button" class="c-textbutton inputfile__remove js-remove">Remove document</button>').insertAfter($label);
                 }
+
             }
             else {
                 resetFile();
             }
+
             $('.js-remove').on("click", function () {
                 $input.val('');
                 resetFile();
             });
         });
 
+       
         // Firefox bug fix
-        $input.on('focus', function () {
-            $input.addClass('has-focus');
-        }).on('blur', function () {
-            $input.removeClass('has-focus');
-        });
-
+        $input
+		.on('focus', function () { $input.addClass('has-focus'); })
+		.on('blur', function () { $input.removeClass('has-focus'); });
     });
 
+   
 })(jQuery, window, document);
